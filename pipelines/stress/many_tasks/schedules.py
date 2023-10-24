579ef6b33c2a346b0872ced175200c0406cda9fe
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-10-24 10:45:13 -0300

    feat: add stress schedule

---
+++ pipelines/stress/many_tasks/schedules.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+
+import pytz
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
+
+every_minute_but_ten_times = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(minutes=1),
+            start_date=datetime(2021, 1, 1, tzinfo=pytz.timezone("America/Sao_Paulo")),
+            labels=["escritoriodedados"],
+            parameter_defaults={
+                "a": "3",
+                "b": "5",
+            },
+        )
+        for _ in range(10)
+    ]
+)
