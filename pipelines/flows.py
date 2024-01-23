f6f06d23ad009aa882fea15dc77ad39f0e5ad8bd
d116626 <d116626@gmail.com>
2024-01-23 16:03:00 -0300

    fix: fix imports

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -3,10 +3,8 @@
 Imports all flows for every project so we can register all of them.
 """
 from pipelines.deteccao_alagamento_cameras import *  # noqa
+from pipelines.dump_url import *  # noqa
 from pipelines.exemplo import *  # noqa
 from pipelines.stress import *  # noqa
 from pipelines.templates import *  # noqa
-from pipelines.waze.dump_alertas import *  # noqa
-from pipelines.dump_url.dump_url_seeketing import *  # noqa
-from pipelines.dump_url.dump_url_turismo import *  # noqa
-
+from pipelines.waze import *  # noqa
