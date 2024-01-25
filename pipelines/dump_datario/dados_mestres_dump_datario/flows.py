963882f3c716de4d389043580b9136fc5b690d9f
KarinaPassos <kaadrain@gmail.com>
2024-01-25 14:51:28 -0300

    setting state handlers

--- pipelines/dump_datario/dados_mestres_dump_datario/flows.py
+++ pipelines/dump_datario/dados_mestres_dump_datario/flows.py
@@ -10,6 +10,7 @@ from prefect.storage import GCS
 
 from pipelines.constants import constants
 from prefeitura_rio.pipelines_templates.dump_datario.flows import flow
+from prefeitura_rio.pipelines_utils.state_handlers import handler_inject_bd_credentials
 
 from pipelines.dump_datario.dados_mestres_dump_datario.schedules import (
     dados_mestresmonthly_update_schedule,
@@ -18,6 +19,7 @@ from pipelines.dump_datario.dados_mestres_dump_datario.schedules import (
 dump_dados_mestres_flow = deepcopy(flow)
 dump_dados_mestres_flow.name = "EMD: dados_mestres - Ingerir tabelas do data.rio"
 dump_dados_mestres_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+dump_dados_mestres_flow.state_handlers = [handler_inject_bd_credentials]
 dump_dados_mestres_flow.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value,
     labels=[constants.RJ_ESCRITORIO_AGENT_LABEL.value],
