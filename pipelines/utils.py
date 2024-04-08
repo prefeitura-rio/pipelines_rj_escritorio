523b1a71c64a497c8b1c14d8d6d3441544b2f118
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2024-04-08 10:42:29 -0300

    fix: inject bd credentials using environment

--- pipelines/utils.py
+++ pipelines/utils.py
@@ -1,8 +1,37 @@
 # -*- coding: utf-8 -*-
+import base64
+from os import environ
 from typing import Any, Callable, Union
 
 import prefect
-from prefeitura_rio.pipelines_utils.infisical import inject_bd_credentials
+from prefeitura_rio.pipelines_utils.infisical import get_infisical_client, inject_env
+from prefeitura_rio.pipelines_utils.logging import log
+from prefeitura_rio.pipelines_utils.prefect import get_flow_run_mode
+
+
+def inject_bd_credentials(environment: str = "staging") -> None:
+    """
+    Loads Base dos Dados credentials from Infisical into environment variables.
+    """
+    client = get_infisical_client()
+
+    log(f"ENVIROMENT: {environment}")
+    for secret_name in [
+        "BASEDOSDADOS_CONFIG",
+        "BASEDOSDADOS_CREDENTIALS_PROD",
+        "BASEDOSDADOS_CREDENTIALS_STAGING",
+    ]:
+        inject_env(
+            secret_name=secret_name,
+            environment=environment,
+            client=client,
+        )
+
+    service_account_name = f"BASEDOSDADOS_CREDENTIALS_{environment.upper()}"
+    service_account = base64.b64decode(environ[service_account_name])
+    with open("/tmp/credentials.json", "wb") as credentials_file:
+        credentials_file.write(service_account)
+    environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/credentials.json"
 
 
 def authenticated_task(
@@ -31,15 +60,11 @@ def authenticated_task(
         """
 
         def new_function(**kwargs):
-            assert "environment" in prefect.context.get(
-                "parameters"
-            ), "Environment not found in flow parameters"
-            logger = prefect.context.get("logger")
-            env = prefect.context.get("parameters")["environment"]
-            logger.debug(f"[Injected] Set BD credentials for environment {env}")
+            env = get_flow_run_mode()
+            log(f"[Injected] Set BD credentials for environment {env}", "debug")
             inject_bd_credentials(environment=env)
 
-            logger.debug("[Injected] Now executing function normally...")
+            log("[Injected] Now executing function normally...", "debug")
             return function(**kwargs)
 
         new_function.__name__ = function.__name__
