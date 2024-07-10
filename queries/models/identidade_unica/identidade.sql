c0294cc06c88963d14b5c88e07c6f49988cfd412
patriciacatandi <patriciabcatandi@gmail.com>
2024-07-10 18:04:59 -0300

    adding cadunico identidade

--- queries/models/identidade_unica/identidade.sql
+++ queries/models/identidade_unica/identidade.sql
@@ -301,3 +301,108 @@ UNION ALL
       CAST(NULL AS STRING) AS cep,
   FROM union_tables
 )
+
+UNION ALL
+
+(
+WITH UltimaDataPorCPF AS (
+    SELECT
+      cpf,
+      MAX(data_particao) AS max_data_particao
+    FROM `rj-smas.protecao_social_cadunico.documento_pessoa`
+    GROUP BY cpf
+  ),
+  UltimaDataIdentificacao AS (
+    SELECT
+      id_pessoa,
+      MAX(data_particao) AS max_data_particao
+    FROM `rj-smas.protecao_social_cadunico.identificacao_primeira_pessoa`
+    GROUP BY id_pessoa
+  ),
+  UltimaIdentificacao AS (
+    SELECT
+      *
+    FROM `rj-smas.protecao_social_cadunico.identificacao_primeira_pessoa` pp
+    INNER JOIN UltimaDataIdentificacao ident ON ident.max_data_particao = pp.data_particao
+      AND ident.id_pessoa = pp.id_pessoa
+  ),
+  UltimaDataControle AS (
+    SELECT
+      id_familia,
+      MAX(data_particao) AS max_data_particao
+    FROM `rj-smas.protecao_social_cadunico.identificacao_controle`
+    GROUP BY 1
+  ),
+  UltimoControle AS (
+    SELECT
+      control.id_familia,
+      CONCAT(tipo_logradouro,
+      logradouro,
+      numero_logradouro,
+      complemento) AS endereco,
+      cep,
+      id_municipio AS municipio -- TODO: trocar
+    FROM `rj-smas.protecao_social_cadunico.identificacao_controle` control
+    INNER JOIN UltimaDataControle ultimo ON ultimo.max_data_particao = control.data_particao
+      AND ultimo.id_familia = control.id_familia
+  ),
+  deficiencia AS (
+    SELECT 
+    ident.id_familia,
+    ident.id_membro_familia,
+    CASE
+      WHEN def.id_familia IS NULL THEN "Não possui"
+      WHEN (
+                IFNULL(CAST(deficiencia_surdez_leve AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_surdez_profunda AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_baixa_visao AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_fisica AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_cegueira AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_mental AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_transtorno_mental AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_sindrome_down AS FLOAT64), 0) 
+            ) = 0 THEN "Não especificado"
+            WHEN (
+                IFNULL(CAST(deficiencia_surdez_leve AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_surdez_profunda AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_baixa_visao AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_fisica AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_cegueira AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_mental AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_transtorno_mental AS FLOAT64), 0) +
+                IFNULL(CAST(deficiencia_sindrome_down AS FLOAT64), 0) 
+            ) > 0 THEN "Deficiência múltipla"
+            WHEN (deficiencia_surdez_profunda IS NOT NULL OR deficiencia_surdez_leve IS NOT NULL) THEN "Deficiência auditiva"
+            WHEN (deficiencia_transtorno_mental IS NOT NULL OR deficiencia_mental IS NOT NULL OR deficiencia_sindrome_down IS NOT NULL) THEN "Deficiência intelectual"
+            WHEN deficiencia_fisica IS NOT NULL THEN "Deficiência física"
+            WHEN (deficiencia_cegueira IS NOT NULL OR deficiencia_baixa_visao IS NOT NULL) THEN "Deficiência visual"
+    END AS deficiencia
+    FROM UltimaIdentificacao ident
+    LEFT JOIN `rj-smas.protecao_social_cadunico.pessoa_deficiencia` def ON ident.id_familia = def.id_familia
+      AND ident.id_membro_familia = def.id_membro_familia
+  )
+  SELECT
+    DISTINCT
+    SAFE_CAST(REGEXP_REPLACE(REGEXP_REPLACE(TRIM(doc.cpf), r'\.0$', ''), r'^0+', '') AS STRING) AS cpf,
+    SHA512(SAFE_CAST(REGEXP_REPLACE(REGEXP_REPLACE(TRIM(doc.cpf), r'\.0$', ''), r'^0+', '') AS STRING)) AS id_hash,
+    UPPER(ident.nome) nome,
+    sexo AS genero,
+    CAST(data_nascimento AS DATE) AS data_nascimento,
+    UPPER(local_nascimento) AS naturalidade,
+    UPPER(pais_nascimento) AS nacionalidade,
+    raca_cor AS raca_cor,
+    def.deficiencia AS deficiencia,
+    CAST(NULL AS STRING) AS bolsa_familia,
+    endereco,
+    CAST(NULL AS STRING) AS bairro,
+    municipio AS municipio,
+    cep AS cep,
+  FROM `rj-smas.protecao_social_cadunico.documento_pessoa` doc
+  INNER JOIN UltimaDataPorCPF cpf ON cpf.max_data_particao = doc.data_particao AND cpf.cpf = doc.cpf
+  INNER JOIN UltimaIdentificacao ident ON doc.id_membro_familia = ident.id_membro_familia
+     AND doc.id_familia = ident.id_familia
+  INNER JOIN UltimoControle control ON control.id_familia = doc.id_familia
+  INNER JOIN deficiencia def ON doc.id_membro_familia = def.id_membro_familia
+     AND doc.id_familia = def.id_familia
+  
+)
